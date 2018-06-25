fd139283b7fa58da52eadb9cab8835c539b6f125
anjan <anjan@w3engineers.com>
2018-06-25 18:49:26 +0600

    text added

--- app/src/main/java/com/aj/user06/customview/MainActivity.java
+++ app/src/main/java/com/aj/user06/customview/MainActivity.java
@@ -5,12 +5,16 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    RevealView revealView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        revealView = findViewById(R.id.reveal_view);
+        revealView.setTitle("RevealView");
+        revealView.setMessage("its a message contain reveal view");
 
 
     }
